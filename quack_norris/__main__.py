00ee6d9d3e25ed675c16b1f80dda8db2cbec3d5a
Michael Fuerst <mail@michaelfuerst.de>
2025-01-21 22:26:29 +0100

    Add initial UI with floating duck and system tray icon

--- quack_norris/__main__.py
+++ quack_norris/__main__.py
@@ -1,21 +1,57 @@
 import sys
+from setproctitle import setproctitle
 from argparse import ArgumentParser
 
 from quack_norris.server.api_server import main as _api_server
-from quack_norris.config import read_config
+from quack_norris.ui.app import main as _create_ui
+from quack_norris.common.config import read_config
 
 
 def ui():
+    setproctitle("quack-norris-ui")
     parser = ArgumentParser("quack-norris-ui")
-    _ = parser.parse_args()
-    raise NotImplementedError()
+    config = read_config("ui.json")
+    parser.add_argument("--debug", action="store_true", help="Enable debug mode")
+    parser.add_argument(
+        "--host",
+        required=False,
+        default=config["host"],
+        type=str,
+        help="A host to overwrite the config temporarily.",
+    )
+    parser.add_argument(
+        "--port",
+        required=False,
+        default=config["port"],
+        type=int,
+        help="A port to overwrite the config temporarily.",
+    )
+
+    args = parser.parse_args()
+    config["host"] = args.host
+    config["port"] = args.port
+    config["debug"] = args.debug
+    _create_ui(config=config)
 
 
 def server():
+    setproctitle("quack-norris-server")
     parser = ArgumentParser("quack-norris-server")
     config = read_config("server.json")
-    parser.add_argument("--host", required=False, default=config["host"], help="A host to overwrite the config temporarily.")
-    parser.add_argument("--port", required=False, default=config["port"], help="A port to overwrite the config temporarily.")
+    parser.add_argument(
+        "--host",
+        required=False,
+        default=config["host"],
+        type=str,
+        help="A host to overwrite the config temporarily.",
+    )
+    parser.add_argument(
+        "--port",
+        required=False,
+        default=config["port"],
+        type=int,
+        help="A port to overwrite the config temporarily.",
+    )
     parser.add_argument("--debug", action="store_true", help="Run the flask server in debug mode.")
     args = parser.parse_args()
     _api_server(host=args.host, port=args.port, debug=args.debug)
