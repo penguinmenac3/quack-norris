9d43f9048eea35cd82e325aee7246802ffd7a656
Michael Fuerst <mail@michaelfuerst.de>
2025-01-22 19:10:09 +0100

    Fix ui startup to run in background again

--- quack_norris/__ui_without_terminal__.py
+++ quack_norris/__ui_without_terminal__.py
@@ -5,6 +5,6 @@ import sys
 def ui():
     args = ' '.join(sys.argv[1:])
     if os.name == 'nt':  # Windows
-        os.system(f"start /b python -m quack_norris ui {args}")
+        os.system(f"start /b pythonw -m quack_norris ui {args}")
     else:  # Linux/Unix/Mac
         os.system(f"python -m quack_norris ui {args} &")
