2c39ccb3d45c7856c938edb2233e5ae989f3f489
Michael Fuerst <mail@michaelfuerst.de>
2025-12-18 00:23:38 +0100

    Fix process calling on linux

--- quack_norris/ui/subprocess_helpers.py
+++ quack_norris/ui/subprocess_helpers.py
@@ -19,10 +19,15 @@ def startup_servers_in_background(dev: bool = False) -> None:
     """Starts the local quack-norris api and ui servers in background processes."""
     root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
     if dev:
+        cmd = LOCAL_UI_CMD
+        if os.name == "nt":
+            cmd = cmd.split()
         p_ui = sp.Popen(LOCAL_UI_CMD.split(), stdout=sp.DEVNULL, cwd=os.path.join(root_dir, 'quack_ui'), shell=True)
         atexit.register(_kill_process, p_ui)
 
     # Add cwd as workspace to quack-norris server
     cmd = LOCAL_API_CMD.split() + ['--workdir', f'{os.getcwd()}']
+    if os.name != "nt":
+        cmd = " ".join(cmd)
     p_api = sp.Popen(cmd, stdout=sp.DEVNULL, cwd=root_dir, shell=True)
     atexit.register(_kill_process, p_api)
